56fe8c7c0980dacf897036e4859af6956eca896a
Viraj Shah <virajshah.77@gmail.com>
2014-08-12 20:27:16 +0530

    LeetCode

--- MaximumSubarray.cpp
+++ MaximumSubarray.cpp
@@ -7,7 +7,8 @@ For example, given the array [-2,1,-3,4,-1,2,1,-5,4],
 the contiguous subarray [4,-1,2,1] has the largest sum = 6.
 
 More practice:
-If you have figured out the O(n) solution, try coding another solution using the divide and conquer approach, which is more subtle.
+If you have figured out the O(n) solution, try coding another solution using the divide and conquer approach,
+which is more subtle.
 
 
 Solution: Keep track of current maximum and compare it with global maximum.
