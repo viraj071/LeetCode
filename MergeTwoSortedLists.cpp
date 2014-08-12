47280cf510c322619bd1537213dd1c09d4cc1295
Viraj Shah <virajshah.77@gmail.com>
2014-08-12 20:28:15 +0530

    LeetCode

--- MergeTwoSortedLists.cpp
+++ MergeTwoSortedLists.cpp
@@ -1,7 +1,8 @@
 /*
 Problem: Merge Two Sorted Lists 
 
-Merge two sorted linked lists and return it as a new list. The new list should be made by splicing together the nodes of the first two lists.
+Merge two sorted linked lists and return it as a new list. The new list should be made by splicing together the nodes
+of the first two lists.
 
 Solution: Basic Merging like in Merge Sort.
 
